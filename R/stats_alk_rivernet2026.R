09f65f92cd20a325cf732f80340b5803fc1130f9
camille.minaudo <camille.minaudo@ub.edu>
2026-03-12 07:10:36 +0000

    edited plot and writing shp output

--- R/stats_alk_rivernet2026.R
+++ R/stats_alk_rivernet2026.R
@@ -12,12 +12,19 @@ st_crs(lakescci)
 
 ggplot(lakescci, aes(alk_mean/1000))+geom_density()+theme_bw()
 
-ggplot(lakescci, aes(alk_mean/1000, fill = type))+geom_density(alpha=0.5)+theme_bw()
 
 # check which lakes are the ones exceeding median value overall
 median_alk <- median(lakescci$alk_mean, na.rm = T)
 
+ggplot(lakescci, aes(alk_mean, fill = type))+
+  geom_density(alpha=0.5)+
+  geom_vline(xintercept = median_alk)+
+  theme_bw()
+
 lakescci_highestAlk <- lakescci[which(lakescci$alk_mean>median_alk),]
 
-sf::write_sf(lakescci_sel, dsn = "PROJECTS/CALCYOM/lakes_cci_database/lakes_cci_preselected.shp", append = F)
+setwd("C:/Users/Camille Minaudo/OneDrive - Universitat de Barcelona/Documentos/")
+sf::write_sf(lakescci_highestAlk, dsn = "PROJECTS/CALCYOM/lakes_cci_database/lakes_cci_highAlk.shp", append = F)
+
+
 
